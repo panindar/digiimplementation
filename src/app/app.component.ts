c7d19d46eabac76ae1310db670b883a290d5bafc
panindar <panindar1819@gmail.com>
2022-07-03 19:50:36 +0530

    added sha encoding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as shajs from 'sha.js';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'digi';
+  clientid = "C12C1CA5";
+  clientSecret = "56116ace7a8d60f585e8";
+  timestamps = Math.floor(Date.now()/1000);
+  data: any = this.clientid + this.clientSecret + this.timestamps;
+  hashValue = shajs('sha256').update(this.data).digest('hex')
+
+
+  constructor() {
+    console.log(this.hashValue)
+    console.log(this.timestamps)
+  }
+
+  getData(res: any) {
+    console.log(res);
+    return "success";
+
+  }
 }
+
