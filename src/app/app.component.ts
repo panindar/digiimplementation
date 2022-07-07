5b9ff97304d1ecd6e1d650e7c1ed573f3c7f086b
panindar <panindar1819@gmail.com>
2022-07-07 12:24:09 +0530

    m

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent {
   title = 'digi';
   clientid: string = "C12C1CA5";
   clientSecret: string = "56116ace7a8d60f585e8";
-  timestamps: string = Math.floor(Date.now()/1000).toString();
+  timestamps: string = Math.floor(new Date().getSeconds()).toString();
   data: string=  this.clientSecret + this.clientid + this.timestamps;
   hashValue: string = shajs('sha256').update(this.data).digest('hex');
 
