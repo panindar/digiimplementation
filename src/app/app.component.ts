a99e9ba2a0f61b46131fd22481d0e3cc52ca7507
panindar <panindar1819@gmail.com>
2022-07-07 13:36:29 +0530

    changed the sequence

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,8 +24,8 @@ export class AppComponent {
   private loadScript() {
     let chatScript = document.createElement("script");
     chatScript.setAttribute("type", "text/javascript");
-    chatScript.setAttribute("id", "dlshare");
     chatScript.setAttribute("src", "https://services.digitallocker.gov.in/requester/api/2/dl.js");
+    chatScript.setAttribute("id", "dlshare");
     chatScript.setAttribute("data-app-id", this.clientid);
     chatScript.setAttribute("data-app-hash", this.hashValue);
     chatScript.setAttribute("time-stamp", this.timestamps);
