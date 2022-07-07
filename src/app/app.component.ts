5906a1e905283bc329d320fa91658cdebc68c509
panindar <panindar1819@gmail.com>
2022-07-07 13:27:48 +0530

    adding to git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import * as shajs from 'sha.js';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,9 +11,13 @@ export class AppComponent {
   title = 'digi';
   clientid: string = "C12C1CA5";
   clientSecret: string = "56116ace7a8d60f585e8";
-  timestamps: string = Math.floor(new Date().getTime()/1000.0).toString();
-  data: string=  this.clientid + this.clientSecret + this.timestamps;
+  timestamps: string = Math.floor(new Date().getTime() / 1000.0).toString();
+  data: string = this.clientSecret + this.clientid + this.timestamps;
   hashValue: string = shajs('sha256').update(this.data).digest('hex');
+  plainText: string;
+
+
+
 
 
 
@@ -26,12 +31,19 @@ export class AppComponent {
     chatScript.setAttribute("time-stamp", this.timestamps);
     chatScript.setAttribute("data-callback", this.getData());
     document.body.appendChild(chatScript);
-    }
+  }
 
   constructor() {
     console.log(this.data)
     console.log(this.timestamps)
     console.log(this.hashValue)
+    var length = 10,
+      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+      this.plainText = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+      this.plainText += charset.charAt(Math.floor(Math.random() * n));
+    }
+    console.log(this.plainText)
     this.loadScript();
   }
 
